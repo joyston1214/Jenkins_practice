47b437f5f88c17101ebd2e99c5365a3f24beed41
fjoys <fjoys@JOYSTON>
2023-06-14 12:17:45 +0530

    first commit

---
+++ Jenkins_Practice/src/test/java/practice/Admin_Module.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Admin_Module {
+
+	@Test
+	public void admin()
+	{
+		System.out.println("successfully created Admin Module");
+		Reporter.log("successfully created Admin Module");
+	}
+}
---
+++ Jenkins_Practice/src/test/java/practice/Login_Module.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Login_Module {
+
+	@Test
+	public void login()
+	{
+		System.out.println("Successfully created Login Module");
+		Reporter.log("Successfully created Login Module");
+	}
+}
